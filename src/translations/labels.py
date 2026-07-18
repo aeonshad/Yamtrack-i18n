475db988ff14e94ae7d57d007744b7108b3c6181
aeonshad <118810850+aeonshad@users.noreply.github.com>
2026-07-18 16:34:20 +0200

    fix(i18n): add missing localization support for import data page

--- src/translations/labels.py
+++ src/translations/labels.py
@@ -4,5 +4,14 @@ from django.utils.translation import gettext_lazy as _
 def ui_labels(request):
     return {
         "ui_labels": {
+            "import": {
+                "once": {
+                    "yamtrack": _("Import from YamTrack backup"),
+                    "games": _("Import games"),
+                    "media": _("Import movies and TV shows from your ratings"),
+                    "books": _("Import from GoodReads backup"),
+                },
+                "periodic": _("File uploads are not available for periodic imports"),
+            },
         }
     }
